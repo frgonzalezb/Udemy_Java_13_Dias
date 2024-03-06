41a44fc0233ed977e451e11f4670d7e03b4e022c
Francisco <fr.gonzalezb@duocuc.cl>
2024-03-06 18:52:27 -0300

    Create Estudiante.java

---
+++ 05_SistemaEstudiantes/src/main/java/dominio/Estudiante.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dominio;
+
+/**
+ *
+ * @author franc
+ */
+public class Estudiante {
+    
+    private int idEstudiante;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+
+    // Para crear un objeto Estudiante vacío
+    public Estudiante() {
+    }
+
+    // Para leer o eliminar un objeto Estudiante
+    public Estudiante(int idEstudiante) {
+        this.idEstudiante = idEstudiante;
+    }
+
+    // Para crear un objeto Estudiante
+    public Estudiante(String nombre, String apellido, String telefono, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    // Para modificar un objeto Estudiante
+    public Estudiante(int idEstudiante, String nombre, String apellido, String telefono, String email) {
+        this.idEstudiante = idEstudiante;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public int getIdEstudiante() {
+        return idEstudiante;
+    }
+
+    public void setIdEstudiante(int idEstudiante) {
+        this.idEstudiante = idEstudiante;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" + "idEstudiante=" + idEstudiante + ", nombre=" + nombre + ", apellido=" + apellido + ", telefono=" + telefono + ", email=" + email + '}';
+    }
+    
+}
