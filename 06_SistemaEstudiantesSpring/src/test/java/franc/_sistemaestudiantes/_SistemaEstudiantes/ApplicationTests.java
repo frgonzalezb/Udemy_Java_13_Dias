1b38e8b30ba234190559925cc0c4d1f925bd04a0
Francisco <fr.gonzalezb@duocuc.cl>
2024-04-02 19:13:06 -0300

    Avance Día 6: Aplicación de Sistema de Estudiantes con Spring Boot y Mysql
    y más!

---
+++ 06_SistemaEstudiantesSpring/src/test/java/franc/_sistemaestudiantes/_SistemaEstudiantes/ApplicationTests.java
@@ -0,0 +1,13 @@
+package franc._sistemaestudiantes._SistemaEstudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
