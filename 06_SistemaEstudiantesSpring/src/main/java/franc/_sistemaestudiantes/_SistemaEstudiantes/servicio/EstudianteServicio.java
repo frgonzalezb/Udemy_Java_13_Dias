16023f088daa6848dd39aa394db2b31de5f59efb
Francisco <fr.gonzalezb@duocuc.cl>
2024-04-03 15:18:57 -0300

    Update EstudianteServicio.java

--- 06_SistemaEstudiantesSpring/src/main/java/franc/_sistemaestudiantes/_SistemaEstudiantes/servicio/EstudianteServicio.java
+++ 06_SistemaEstudiantesSpring/src/main/java/franc/_sistemaestudiantes/_SistemaEstudiantes/servicio/EstudianteServicio.java
@@ -1,4 +1,41 @@
+// Importante agregar @Service para que Spring reconozca la clase como un servicio
+// @Autowired para inyectar dependencias de la fábrica de Spring
+
 package franc._sistemaestudiantes._SistemaEstudiantes.servicio;
 
+import franc._sistemaestudiantes._SistemaEstudiantes.modelo.Estudiante;
+
+import franc._sistemaestudiantes._SistemaEstudiantes.repositorio.EstudianteRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EstudianteServicio implements IEstudianteServicio {
+
+    @Autowired
+    private EstudianteRepositorio estudianteRepositorio;
+
+    @Override
+    public List<Estudiante> listarEstudiantes() {
+        List<Estudiante> estudiantes = estudianteRepositorio.findAll();
+        return estudiantes;
+    }
+
+    @Override
+    public Estudiante buscarEstudiantePorId(Integer idEstudiante) {
+        Estudiante estudiante = estudianteRepositorio.findById(idEstudiante).orElse(null);
+        return estudiante;
+    }
+
+    @Override
+    public void guardarEstudiante(Estudiante estudiante) {
+        estudianteRepositorio.save(estudiante);
+    }
+
+    @Override
+    public void eliminarEstudiante(Estudiante estudiante) {
+        estudianteRepositorio.delete(estudiante);
+    }
 }
