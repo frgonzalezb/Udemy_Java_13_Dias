d75a19596c0e0022525c687f4b8fa7f56f1cc786
Francisco <fr.gonzalezb@duocuc.cl>
2024-07-31 18:01:44 -0400

    Configuración Inicial de JavaFX

---
+++ 08_Tareas/src/main/java/franc/tasks/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package franc.tasks.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class IndexController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
