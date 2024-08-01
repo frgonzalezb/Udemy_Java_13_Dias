a12d98c9873251b53f6f220e66c43219b892e471
Francisco <fr.gonzalezb@duocuc.cl>
2024-08-01 14:35:03 -0400

    Listado de Tareas con JavaFX y Spring

--- 08_Tareas/src/main/java/franc/tasks/controllers/IndexController.java
+++ 08_Tareas/src/main/java/franc/tasks/controllers/IndexController.java
@@ -1,6 +1,18 @@
 package franc.tasks.controllers;
 
+import franc.tasks.models.Task;
+import franc.tasks.services.TaskService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
@@ -8,8 +20,39 @@ import java.util.ResourceBundle;
 
 @Component
 public class IndexController implements Initializable {
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @Autowired
+    private TaskService taskService;
+
+    @FXML
+    private TableView<Task> taskTable;
+
+    @FXML
+    private TableColumn<Task, Long> idCol;
+    @FXML
+    private TableColumn<Task, String> titleCol;
+    @FXML
+    private TableColumn<Task, String> workerCol;
+    @FXML
+    private TableColumn<Task, String> priorityCol;
+    @FXML
+    private TableColumn<Task, String> statusCol;
+
+    private final ObservableList<Task> tasks = FXCollections.observableArrayList();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        taskTable.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        configColumns();
+    }
 
+    private void configColumns() {
+        idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        titleCol.setCellValueFactory(new PropertyValueFactory<>("title"));
+        workerCol.setCellValueFactory(new PropertyValueFactory<>("workerInCharge"));
+        priorityCol.setCellValueFactory(new PropertyValueFactory<>("priority"));
+        statusCol.setCellValueFactory(new PropertyValueFactory<>("status"));
+    }
     }
 }
