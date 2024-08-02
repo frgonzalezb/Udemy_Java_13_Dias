8168e46f73b5f703d5e5842ea9492e7a1c51afe0
Francisco <fr.gonzalezb@duocuc.cl>
2024-08-02 16:34:06 -0400

    Proyecto Inicial con Spring

---
+++ 09_SistemaEmpleados/src/main/java/franc/simpleHRMS/SimpleHrmsApplication.java
@@ -0,0 +1,13 @@
+package franc.simpleHRMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleHrmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleHrmsApplication.class, args);
+	}
+
+}
---
+++ 09_SistemaEmpleados/src/test/java/franc/simpleHRMS/SimpleHrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package franc.simpleHRMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleHrmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
