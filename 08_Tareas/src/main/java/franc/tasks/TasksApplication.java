06edae4dbfb20ab642bb67abc69bf434705dcffc
Francisco <fr.gonzalezb@duocuc.cl>
2024-08-01 22:38:30 -0400

    Update TasksApplication.java

--- 08_Tareas/src/main/java/franc/tasks/TasksApplication.java
+++ 08_Tareas/src/main/java/franc/tasks/TasksApplication.java
@@ -2,14 +2,12 @@ package franc.tasks;
 
 import franc.tasks.views.TaskView;
 import javafx.application.Application;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TasksApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(TasksApplication.class, args);
 		Application.launch(TaskView.class, args);
 	}
 
