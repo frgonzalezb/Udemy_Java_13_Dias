01ee62b2724d2a905fa30c4a44cc12a58749cb7f
Francisco <fr.gonzalezb@duocuc.cl>
2024-08-02 18:09:51 -0400

    Creación Clase de Entidad de Empleado con JPA-Hibernate

---
+++ 09_SistemaEmpleados/src/main/java/franc/simpleHRMS/models/Employee.java
@@ -0,0 +1,29 @@
+package franc.simpleHRMS.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String jobTitle;
+    private String department;
+    private String email;
+    private String phone;
+    private Double grossPayUSD;
+    private boolean isActive;
+    private boolean isDeleted;
+}
