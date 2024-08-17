9405460c9613b43828fa5490e36384cb7f11a6b9
Francisco <fr.gonzalezb@duocuc.cl>
2024-08-17 13:57:30 -0400

    Listado y Clase de Servicio con Angular

--- 12_SistemaInventarios/Frontend/inventory-mgmt-app/src/app/components/product-list/product-list.component.ts
+++ 12_SistemaInventarios/Frontend/inventory-mgmt-app/src/app/components/product-list/product-list.component.ts
@@ -1,12 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+  products: Product[];
 
+  constructor(private productService: ProductService) {
+    this.products = [];
+  }
+
+  ngOnInit() {
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
+    this.productService.getAllProducts().subscribe((products) => {
+      this.products = products;
+    })
+  }
 }
