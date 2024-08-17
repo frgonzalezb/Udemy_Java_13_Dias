09a67a950806ee9453102ef4882eafe66da8c1db
Francisco <fr.gonzalezb@duocuc.cl>
2024-08-17 14:25:29 -0400

    Listado de Productos con Angular

--- 12_SistemaInventarios/Frontend/inventory-mgmt-app/src/app/app.component.ts
+++ 12_SistemaInventarios/Frontend/inventory-mgmt-app/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductListComponent],
+  imports: [
+    RouterOutlet,
+    HttpClientModule,
+    RouterModule,
+    ProductListComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- 12_SistemaInventarios/Frontend/inventory-mgmt-app/src/app/components/product-list/product-list.component.ts
+++ 12_SistemaInventarios/Frontend/inventory-mgmt-app/src/app/components/product-list/product-list.component.ts
@@ -2,11 +2,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../models/product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [CommonModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
