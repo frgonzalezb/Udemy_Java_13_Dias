564f306a2b018e606ba8b0239b4f73a5b726726e
Francisco <fr.gonzalezb@duocuc.cl>
2024-08-19 15:41:45 -0400

    Edición personal - Agregar footer

--- 12_SistemaInventarios/Frontend/inventory-mgmt-app/src/app/app.component.ts
+++ 12_SistemaInventarios/Frontend/inventory-mgmt-app/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './components/common/navbar/navbar.component';
+import { FooterComponent } from "./components/common/footer/footer.component";
 
 @Component({
   selector: 'app-root',
@@ -12,8 +13,9 @@ import { NavbarComponent } from './components/common/navbar/navbar.component';
     RouterModule,
     HttpClientModule,
     NavbarComponent,
-    ProductListComponent
-  ],
+    ProductListComponent,
+    FooterComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
