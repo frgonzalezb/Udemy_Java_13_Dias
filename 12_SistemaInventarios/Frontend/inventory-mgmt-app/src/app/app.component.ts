2b7dac008ec27cbe00a907975b25989e9bdb3ecf
Francisco <fr.gonzalezb@duocuc.cl>
2024-08-19 13:04:01 -0400

    Edición propia - Trasladar navbar a componente propio

--- 12_SistemaInventarios/Frontend/inventory-mgmt-app/src/app/app.component.ts
+++ 12_SistemaInventarios/Frontend/inventory-mgmt-app/src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/common/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    HttpClientModule,
     RouterModule,
+    HttpClientModule,
+    NavbarComponent,
     ProductListComponent
   ],
   templateUrl: './app.component.html',
