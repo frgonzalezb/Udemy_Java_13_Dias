3356f6a0ae46269e851d77a69a84a6e358f4915b
Francisco <fr.gonzalezb@duocuc.cl>
2024-08-19 16:05:13 -0400

    Caso de Agregar con Spring (Back End)

--- 12_SistemaInventarios/Backend/src/main/java/franc/inventoryManagement/controllers/ProductController.java
+++ 12_SistemaInventarios/Backend/src/main/java/franc/inventoryManagement/controllers/ProductController.java
@@ -5,9 +5,7 @@ import franc.inventoryManagement.services.IProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,19 +15,26 @@ public class ProductController {
     private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
 
     @Autowired
-    private IProductService iProductService;
+    private IProductService productService;
 
     private List<Product> products;
 
     @GetMapping("/products")
     public List<Product> getAllProducts() {
         logger.info("ProductController getAllProducts() called.");
-        this.products = iProductService.listAllProducts()
+        this.products = productService.listAllProducts()
                 .stream()
                 .filter(Product::isActive)
                 .toList();
         this.products.forEach(product -> logger.info(product.toString()));
         logger.info("Products have been listed successfully.");
         return this.products;
+    }
+
+    @PostMapping("/products")
+    public Product saveProduct(@RequestBody Product product) {
+        logger.info("ProductController saveProduct() called.");
+        logger.info("Product: " + product.toString());
+        return productService.saveProduct(product);
     }
 }
