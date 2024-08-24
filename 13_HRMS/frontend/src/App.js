a5d22c2425b99a77c38f1cdb783b5d7f5ad2262d
Francisco <fr.gonzalezb@duocuc.cl>
2024-08-23 22:13:22 -0400

    Componente de Rutas en React

--- 13_HRMS/frontend/src/App.js
+++ 13_HRMS/frontend/src/App.js
@@ -1,11 +1,18 @@
 import Navbar from "./components/common/navbar";
 import ListEmployees from "./components/employees/list";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div className="App container text-center">
-      <Navbar/>
-      <ListEmployees/>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<ListEmployees/>} />
+          <Route path="/add-employee" element={<ListEmployees/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
