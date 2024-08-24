a7cb7feac0df0ed299abdf104744d8cac3df7ee3
Francisco <fr.gonzalezb@duocuc.cl>
2024-08-23 22:05:59 -0400

    Componente de Navegación con React y Bootstrap

--- 13_HRMS/frontend/src/App.js
+++ 13_HRMS/frontend/src/App.js
@@ -1,8 +1,10 @@
+import Navbar from "./components/common/navbar";
 import ListEmployees from "./components/employees/list";
 
 function App() {
   return (
     <div className="App container text-center">
+      <Navbar/>
       <ListEmployees/>
     </div>
   );
